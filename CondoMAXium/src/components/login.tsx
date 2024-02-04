48808f80c999f70b9d84a36ce64c8cb0ada04b60
El-Mirrio <116108802+El-Mirrio@users.noreply.github.com>
2024-02-04 17:31:38 -0500

    Fixed Merge

--- CondoMAXium/src/components/login.tsx
+++ CondoMAXium/src/components/login.tsx
@@ -17,7 +17,6 @@ const Login = () => {
       <div className="submit-container">
         <div className="submit">Login</div>
         <div className="submit"></div>Create New Account</div>
-      </div>
     </div>
   );
 };
