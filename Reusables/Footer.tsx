030636b89ae8714723c3f796a48c8e90efb4f813
El-Mirrio <116108802+El-Mirrio@users.noreply.github.com>
2024-02-05 19:12:59 -0500

    Add files via upload
    Header Footer Layout

---
+++ Reusables/Footer.tsx
@@ -0,0 +1,12 @@
+// Footer.tsx
+import React from 'react';
+
+const Footer: React.FC = () => {
+    return (
+        <footer style={{ backgroundColor: 'black', padding: '40px', textAlign: 'center' }}>
+            <p>&copy; 2024 Your App Name</p>
+        </footer>
+    );
+};
+
+export default Footer;
