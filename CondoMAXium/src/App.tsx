a7118b4c72373c712a264d5b1b82659090c90ae3
El-Mirrio <116108802+El-Mirrio@users.noreply.github.com>
2024-02-07 14:30:06 -0500

    signup page added to App

--- CondoMAXium/src/App.tsx
+++ CondoMAXium/src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './Layout';
 import Login from './components/login';
 import Profile from './components/profile';
+import SignUp from "./components/signup";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Login />} />
+                <Route path="/signup" element={<SignUp />} />
                 <Route path="/profile" element={<Layout><Profile /></Layout>} />
                 <Route path="*" element={<div>You lost?</div>} />
             </Routes>
