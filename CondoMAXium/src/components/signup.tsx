d06ca6c3835eb99ade5b5c8ad123b64618b0f82d
Nicola-Bdewi <74018961+Nicola-Bdewi@users.noreply.github.com>
2024-02-27 18:33:11 -0500

    Brackets problem solved when exporting

--- CondoMAXium/src/components/signup.tsx
+++ CondoMAXium/src/components/signup.tsx
@@ -19,25 +19,7 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-// function Copyright(props: any) {
-//   return (
-//     <Typography
-//       variant="body2"
-//       color="text.secondary"
-//       align="center"
-//       {...props}
-//     >
-//       {"Copyright © "}
-//       <Link color="inherit" href="https://mui.com/">
-//         Your Website
-//       </Link>{" "}
-//       {new Date().getFullYear()}
-//       {"."}
-//     </Typography>
-//   );
-// }
 
-// TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
 interface SignUpResponse {
