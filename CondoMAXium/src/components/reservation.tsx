cfc81d74784d6d97eafded25378db360e21f1ab0
dlu2002 <al_delu@live.concordia.ca>
2024-03-01 23:45:06 -0500

    Updated App.tsx
    Added new paths to App.tsx

--- CondoMAXium/src/components/reservation.tsx
+++ CondoMAXium/src/components/reservation.tsx
@@ -5,7 +5,6 @@ import { format, isBefore, startOfDay, addMonths } from 'date-fns';
 import { FaCalendarAlt } from 'react-icons/fa';
 import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import MuiDrawer from '@mui/material/Drawer';
 import Box from '@mui/material/Box';
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
