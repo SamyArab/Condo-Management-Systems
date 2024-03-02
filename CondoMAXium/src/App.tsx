e6f1f5746981d5a8031ef3f73d88619b38d33a47
dlu2002 <al_delu@live.concordia.ca>
2024-03-01 23:48:25 -0500

    Update App.tsx

--- CondoMAXium/src/App.tsx
+++ CondoMAXium/src/App.tsx
@@ -5,6 +5,8 @@ import Login from './components/login';
 import Profile from './components/profile';
 import SignUp from "./components/signup";
 import ReservationPage from './components/reservation';
+import FormReservation from "./components/form-reservation";
+import MyReservations from "./components/my-reservations";
 
 function App() {
     return (
@@ -14,6 +16,8 @@ function App() {
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/reservation" element={<ReservationPage />} />
                 <Route path="/profile" element={<Layout><Profile /></Layout>} />
+                <Route path="/reserve" element={<FormReservation />} />
+                <Route path="/my-reservations" element={<MyReservations/>}/>
                 <Route path="*" element={<div>You lost?</div>} />
             </Routes>
         </BrowserRouter>
