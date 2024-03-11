c667cc429395938305aa1ad3ceda97cacfd9cedb
SamyArab <samy013@outlook.com>
2024-03-10 23:04:15 -0400

    reseting to Next.js structure

--- CondoMAXium/src/components/form-reservation.tsx
+++
@@ -1,8 +0,0 @@
-import React, {useEffect, useRef, useState} from "react";
-
-
-const FormReservation: React.FC = () => {
-    return null;
-};
-
-export default FormReservation;
--- CondoMAXium/src/components/my-reservations.tsx
+++
@@ -1,8 +0,0 @@
-import React, {useEffect, useRef, useState} from "react";
-
-
-const MyReservations: React.FC = () => {
-    return null;
-};
-
-export default MyReservations;
