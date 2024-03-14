d355fa19f0ccc907a6e7a3df07e6efa69e9dbda5
mkandaleft <markandaleft@hotmail.com>
2024-03-14 17:49:36 -0400

    added googleSignup page

---
+++ CondoMAXium/pages/signupGoogle/index.js
@@ -0,0 +1,141 @@
+import React from "react";
+import supabase from "../../config/supabaseClient";
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const defaultTheme = createTheme();
+
+// Sign up with Google in order to get their first and last name similar to signup
+const SignUpGoogle = () => {
+    
+  const [first_name, setFirstName] = useState("");
+  const [last_name, setLastName] = useState("");
+
+
+/*
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data, error } = await supabase.auth.signUp({
+        // Added additional info to sign up
+        options: {
+          data: {
+            first_name: first_name,
+            last_name: last_name,
+          },
+        },
+      });
+
+      if (error?.status === 429) {
+        alert("Error signing up: Email rate limit exceeded");
+        throw error;
+      }
+
+      console.log("User signed up successfully:", data);
+      <Link href="/"></Link>;
+      // routeChange();
+    } catch (error) {
+      console.error("Error signing up:", error.message);
+    }
+  };
+*/
+  const router = useRouter();
+  
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  value={first_name}
+                  onChange={(event) => setFirstName(event.target.value)}
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                  value={last_name}
+                  onChange={(event) => setLastName(event.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={
+                    <Checkbox value="allowExtraEmails" color="primary" />
+                  }
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              onClick={() => router.push("/profile")}
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="/" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        {/* <Copyright sx={{ mt: 5 }} /> */}
+      </Container>
+    </ThemeProvider>
+  );
+};
+export default SignUpGoogle;
