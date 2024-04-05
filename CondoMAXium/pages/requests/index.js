842b8fe3bbf2df8f1ef0ac04638b65f149424b7d
thaneekan <92413776+thaneekan@users.noreply.github.com>
2024-04-04 20:09:50 -0400

    basic setup for new page and branch

---
+++ CondoMAXium/pages/requests/index.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiDrawer from "@mui/material/Drawer";
+import Box from "@mui/material/Box";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import Badge from "@mui/material/Badge";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import HomeIcon from "@mui/icons-material/Home";
+import Button from "@mui/material/Button";
+import { useMediaQuery } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+
