f7e612b7ca7e277216945a383a48ffe62df7a814
raccoonrick <rjmungcal@gmail.com>
2024-04-06 17:48:08 -0400

    Added front end to reset-password and  user-verification

--- CondoMAXium/pages/user-verification/index.js
+++ CondoMAXium/pages/user-verification/index.js
@@ -3,20 +3,18 @@ import supabase from "../../config/supabaseClient";
 import { useState } from "react";
 import { useRouter } from "next/router";
 
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { WindowSharp } from "@mui/icons-material";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import KeyOutlinedIcon from '@mui/icons-material/KeyOutlined';
+import Avatar from "@mui/material/Avatar";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+const defaultTheme = createTheme();
 
 function VerifyOTP() {
   const [email, setEmail] = useState("");
@@ -41,20 +39,69 @@ function VerifyOTP() {
   };    
 
   return (
-    <div>
-      <h1>Verify OTP</h1>
-      <form onSubmit={handleVerify}>
-        <label>
-          Email:
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-        </label>
-        <label>
-          OTP:
-          <input type="text" value={otp} onChange={(e) => setOtp(e.target.value)} required />
-        </label>
-        <button type="submit">Verify</button>
-      </form>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth ="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop:20,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}>
+            <Avatar sx={{m: 1, bgcolor: "secondary.main"}}>
+              <KeyOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant='h5'>
+              Verify One Time Password
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleVerify} sx={{mt: 3}}>
+              <Grid container spacing={4}>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    type='email'
+                    label='Email Address'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)} />
+                </Grid>
+                <Grid item xs={12} >
+                  <TextField
+                    required
+                    fullWidth
+                    type="text"
+                    label="One Time Password"
+                    value={otp}
+                    onChange={(e) => setOtp(e.target.value)} />
+                </Grid>
+              </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant='contained'
+                onClick={handleVerify}
+                sx={{mt: 3, mb: 2}}>
+                  Verify
+                </Button>
+            </Box>
+          </Box>
+      </Container>
+    </ThemeProvider>
+     /* <div>
+       <h1>Verify OTP</h1>
+       <form onSubmit={handleVerify}>
+         <label>
+           Email:
+           <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+         </label>
+         <label>
+           OTP:
+           <input type="text" value={otp} onChange={(e) => setOtp(e.target.value)} required />
+         </label>
+         <button type="submit">Verify</button>
+       </form>
+     </div> */
   );
 }
 
