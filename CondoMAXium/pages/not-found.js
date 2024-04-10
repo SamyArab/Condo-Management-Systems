4c6f559181e72fddd5536ffd1613fae96b3d275c
SamyArab <samy013@outlook.com>
2024-04-10 16:58:45 -0400

    added dynamic routing for 404 error

--- CondoMAXium/pages/not-found.js
+++ CondoMAXium/pages/not-found.js
@@ -1,3 +1,12 @@
-export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>;
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1>Not found – 404!</h1>
+      <div>
+        <Link href="/">Go back to Home</Link>
+      </div>
+    </div>
+  );
 }
