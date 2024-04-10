8350fea757632c7262f9269a93bb6bc3ef3cc4bb
Karinsarkis98 <karinsarkis125@gmail.com>
2024-04-10 01:20:40 -0400

    backend for requests and add-request pages

--- CondoMAXium/pages/add-request/index.js
+++ CondoMAXium/pages/add-request/index.js
@@ -20,101 +20,113 @@ import {
     data:{user}
   }=await supabase.auth.getUser();
 
-  const handleAdd = async (event) => {
-    event.preventDefault();
+  const AddRequestForm = () => {
+    const router = useRouter();
 
-    try {
-      const { data, error } = await supabase.from("units").insert([
-        {
-          request_subject: requestSubject,
-          request_type: requestType,
-          request_description: requestDescription,
-        },
-      ]);
-
-      console.log("Successfully added request: ", data);
-      router.push("/requests");
-    } catch (error) {
-      console.error("Error adding unit: ", error.message);
-    }
-
-  };
-
-  const AddRequestForm =()=> {
     const [requestSubject, setRequestSubject] = useState("");
     const [requestType, setRequestType] = useState("");
     const [requestDescription, setRequestDescription] = useState("");
+    const [userr, setUser] = useState("kAR");
+    //const [statuss, setStatus] = useState("");
+    //const [assign, setAssign] = useState("");
 
-    const router = useRouter();
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+  
+      try {
+        const { data, error } = await supabase.from("requests").insert([
+          {
+            subject: requestSubject,
+            type: requestType,
+            description: requestDescription,
+            user: userr,
+            //status: statuss,
+            //assigned_to: assign,
+          },
+        ]);
+        console.log(requestSubject, requestType, requestDescription);
+        console.log(requestSubject, requestType, requestDescription);
+  
+        console.log("Successfully added request: ", data);
+        router.push("/requests");
+      } catch (error) {
+        console.error("Error adding request: ", error.message);
+      }
 
-    return (
-        <Box className={styles.outsideContainer}>
-            <Container className={styles.unitsContainer}>
-            <Typography variant="h4" gutterBottom className={styles.editUnitsHeader}>
-              Create Request
-            </Typography>
-            {/* <form onSubmit={handleAdd}> */}
-              <Grid container spacing={2} alignItems="center">
-                <Grid item xs={6}>
-                  <TextField
-                    name="requestSubject"
-                    label="Request Subject"
-                    value={requestSubject}
-                    onChange={(event) => setRequestSubject(event.target.value)}
-                    fullWidth
-                    margin="normal"
-                    required
-                  />
+  
+    };
+  
+      return (
+          <Box className={styles.outsideContainer}>
+              <Container className={styles.unitsContainer}>
+              <Typography variant="h4" gutterBottom className={styles.editUnitsHeader}>
+                Create Request
+              </Typography>
+              {/* <form onSubmit={handleSubmit}> */}
+                <Grid container spacing={2} alignItems="center">
+                  <Grid item xs={6}>
+                    <TextField
+                      name="requestSubject"
+                      label="Request Subject"
+                      value={requestSubject}
+                      onChange={(event) => setRequestSubject(event.target.value)}
+                      fullWidth
+                      margin="normal"
+                      required
+                    />
+                  </Grid>
+                  {/* <Grid item xs={6}></Grid> */}
+                  <Grid item xs={6}>
+                    <FormControl required fullWidth variant="outlined" margin="normal">
+                      <InputLabel id="request-type-label">Request Type</InputLabel>
+                      <Select
+                        labelId="request-type-label"
+                        id="request-type-select"
+                        value={requestType}
+                        onChange={(event) => setRequestType(event.target.value)}
+                        label="Request Type"
+                      >
+                        <MenuItem value="Maintenance">Maintenance</MenuItem>
+                        <MenuItem value="Access">Access</MenuItem>
+                        <MenuItem value="Violation">Violation</MenuItem>
+                        <MenuItem value="Renovation">Renovation</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Grid>
+      
+                  <Grid item xs={12}>
+                    <TextField
+                      name="requestDescription"
+                      label="Description"
+                      value={requestDescription}
+                      onChange={(event) => setRequestDescription(event.target.value)}
+                      fullWidth
+                      margin="normal"
+                      required
+                      multiline
+                      rows={12}
+                      variant="outlined"
+                    />
+                  </Grid>
                 </Grid>
-                {/* <Grid item xs={6}></Grid> */}
-                <Grid item xs={6}>
-                  <FormControl required fullWidth variant="outlined" margin="normal">
-                    <InputLabel id="request-type-label">Request Type</InputLabel>
-                    <Select
-                      labelId="request-type-label"
-                      id="request-type-select"
-                      value={requestType}
-                      onChange={(event) => setRequestType(event.target.value)}
-                      label="Request Type"
-                    >
-                      <MenuItem value="Maintenance">Maintenance</MenuItem>
-                      <MenuItem value="Access">Access</MenuItem>
-                      <MenuItem value="Violation">Violation</MenuItem>
-                      <MenuItem value="Renovation">Renovation</MenuItem>
-                    </Select>
-                  </FormControl>
+                <Grid container justifyContent="center" style={{ marginTop: "20px" }}>
+                  <Button
+                    variant="contained" 
+                    size="large"
+                    onClick={handleSubmit}
+                    // onClick={() => router.push("/units")}
+                  >
+                    Submit
+                  </Button>
                 </Grid>
+              {/* </form> */}
+            </Container>
+          </Box>
+        );
     
-                <Grid item xs={12}>
-                  <TextField
-                    name="requestDescription"
-                    label="Description"
-                    value={requestDescription}
-                    onChange={(event) => setRequestDescription(event.target.value)}
-                    fullWidth
-                    margin="normal"
-                    required
-                    multiline
-                    rows={12}
-                    variant="outlined"
-                  />
-                </Grid>
-              </Grid>
-              <Grid container justifyContent="center" style={{ marginTop: "20px" }}>
-                <Button
-                  variant="contained" 
-                  size="large"
-                  onClick={handleAdd}
-                  // onClick={() => router.push("/units")}
-                >
-                  Save
-                </Button>
-              </Grid>
-            {/* </form> */}
-          </Container>
-        </Box>
-      );
-  }
+
+  };
+
 
   export default AddRequestForm;
 
--- CondoMAXium/pages/requests/index.js
+++ CondoMAXium/pages/requests/index.js
@@ -1,5 +1,7 @@
 import * as React from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import supabase from "../../config/supabaseClient";
 
 import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -24,97 +26,102 @@ import AddIcon from "@mui/icons-material/Add";
 import Link from "@mui/material/Link";
 
 const drawerWidth = 240;
-const defaultTheme = createTheme(); 
+const defaultTheme = createTheme();
 function Copyright(props) {
-    return (
-      <Typography
-        variant="body2"
-        color="text.secondary"
-        align="center"
-        {...props}
-      >
-        {"Copyright © "}
-        <Link color="inherit" href="https://mui.com/">
-          CondoMAXium
-        </Link>{" "}
-        {new Date().getFullYear()}
-        {"."}
-      </Typography>
-    );
-  }
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        CondoMAXium
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
-  const statusStyles = {
-    Resolved: {
-      backgroundColor: 'green',
-      color: 'white',
-    },
-    "In Progress": {
-      backgroundColor: 'blue',
-      color: 'white',
-    },
-    Open: {
-      backgroundColor: 'orange',
-      color: 'white',
-    },
-  };
+const statusStyles = {
+  Resolved: {
+    backgroundColor: 'green',
+    color: 'white',
+  },
+  "In Progress": {
+    backgroundColor: 'blue',
+    color: 'white',
+  },
+  Open: {
+    backgroundColor: 'orange',
+    color: 'white',
+  },
+};
 
 const AppBar = styled(MuiAppBar, {
-    shouldForwardProp: prop => prop !== "open"
+  shouldForwardProp: prop => prop !== "open"
 })(({ theme, open }) => ({
-    zIndex: theme.zIndex.drawer + 1,
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
     transition: theme.transitions.create(["width", "margin"], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen
-    }),
-    ...(open && {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(["width", "margin"], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen
-        })
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
     })
+  })
 }));
 
 // Custom styled Drawer component
 const Drawer = styled(MuiDrawer, {
-    shouldForwardProp: prop => prop !== "open"
+  shouldForwardProp: prop => prop !== "open"
 })(({ theme, open }) => ({
-    "& .MuiDrawer-paper": {
-        position: "relative",
-        whiteSpace: "nowrap",
-        width: open ? drawerWidth : 0, // Adjust width based on open state
-        transition: theme.transitions.create("width", {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen
-        }),
-        boxSizing: "border-box",
-        overflowX: "hidden", // Hide content overflow when closed
-        ...(open && {
-            // Ensure drawer remains open on larger screens
-            [theme.breakpoints.up("sm")]: {
-                width: drawerWidth,
-                position: "relative"
-            }
-        })
-    }
+  "& .MuiDrawer-paper": {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: open ? drawerWidth : 0, // Adjust width based on open state
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    }),
+    boxSizing: "border-box",
+    overflowX: "hidden", // Hide content overflow when closed
+    ...(open && {
+      // Ensure drawer remains open on larger screens
+      [theme.breakpoints.up("sm")]: {
+        width: drawerWidth,
+        position: "relative"
+      }
+    })
+  }
 }));
 
-function StatusIndicator({ status }) {
-    const paddingHorizontal=2;
-    const paddingVertical=1;
-    const minWidth=100;
+
+const userRequests = () => {
+  
+  const [requests, setRequests] = useState([]);
+
+  function StatusIndicator({ status }) {
+    const paddingHorizontal = 2;
+    const paddingVertical = 1;
+    const minWidth = 100;
 
     return (
       <Box
         sx={{
           ...statusStyles[status],
           borderRadius: '20px', // Rounded corners
-          alignItems:"center",
-          justifyContent:"center",
+          alignItems: "center",
+          justifyContent: "center",
           pX: paddingHorizontal,
-          pY:paddingVertical,
-          minWidth:minWidth,
+          pY: paddingVertical,
+          minWidth: minWidth,
           display: 'inline-flex', // Make the box wrap its content
         }}
       >
@@ -125,152 +132,174 @@ function StatusIndicator({ status }) {
     );
   }
 
-export default function Requests() {
-    const [open, setOpen] = React.useState(true);
-    const router = useRouter();
-    const toggleDrawer = () => {
-      setOpen(!open);
-    };
 
-    const rows = [
-        {id: 1, Subject:"Row1 Data 1", Type: 'Row 1 Data 2', AssignedTo: 'Row 1 Data 3', status: 'Resolved' },
-        { id: 2, Subject:"Row2 Data 1", Type: 'Row 2 Data 2', AssignedTo: 'Row 2 Data 3', status: 'In Progress' },
-        { id: 3, Subject:"Row3 Data 1", Type: 'Row 3 Data 2', AssignedTo: 'Row 3 Data 3', status: 'Open' },
-        { id: 4, Subject:"Row4 Data 1", Type: 'Row 4 Data 2', AssignedTo: 'Row 4 Data 3', status: 'In Progress' },
-        { id: 5, Subject:"Row5 Data 1", Type: 'Row 5 Data 2', AssignedTo: 'Row 5 Data 3', status: 'Resolved' },
-        { id: 6, Subject:"Row6 Data 1", Type: 'Row 6 Data 2', AssignedTo: 'Row 6 Data 3', status: 'Resolved' },
-        // Add more rows as needed
-      ];
-  
-    return (
-      <ThemeProvider theme={defaultTheme}>
-        <Box sx={{ display: "flex" }}>
-          <CssBaseline />
-          <AppBar position="absolute" open={open}>
-            <Toolbar
-              sx={{
-                pr: "24px", // keep right padding when drawer closed
-              }}
-            >
-              <IconButton
-                edge="start"
-                color="inherit"
-                aria-label="open drawer"
-                onClick={toggleDrawer}
-                sx={{
-                  marginRight: "36px",
-                  ...(open && { display: "none" }),
-                }}
-              >
-                <MenuIcon/>
-              </IconButton>
-              <Typography
-                component="h1"
-                variant="h6"
-                color="inherit"
-                noWrap
-                sx={{ flexGrow: 1 }}
-              >
-                My Requests
-              </Typography>
-              <IconButton color="inherit">
-                <Badge badgeContent={4} color="secondary">
-                  <NotificationsIcon />
-                </Badge>
-              </IconButton>
-            </Toolbar>
-          </AppBar>
-          <Drawer variant="permanent" open={open}>
-            <Toolbar
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "flex-end",
-                px: [1],
-              }}
-            >
-              <IconButton aria-label="close drawer" onClick={toggleDrawer}>
-                <ChevronLeftIcon />
-              </IconButton>
-            </Toolbar>
-            <Divider />
-            <List component="nav">
-              {/* Your existing list items */}
-              <ListItem
-                button
-                aria-label="add request"
-                onClick={() => router.push("/add-request")}
-              >
-                <ListItemIcon>
-                  <AddIcon /> {/* Icon for adding */}
-                </ListItemIcon>
-                <ListItemText primary="Add Request" />
-              </ListItem>
-            </List>
-          </Drawer>
-          <Box
-            component="main"
+  //const [requests, setRequests] = useState([]);
+  //const [searchTerm, setSearchTerm] = useState("");
+
+  //fetching units from database
+  useEffect(() => {
+    async function fetchRequests() {
+      // const {
+      //   data: { user },
+      // } = await supabase.auth.getUser();
+      
+      // // Get the user's ID
+      // const userId = user?.id;
+
+      try {
+        //const { data, error } = await supabase.from('requests').select('*').eq("id", userId);
+        const { data, error } = await supabase.from('requests').select('*');
+        setRequests(data);
+        if (error) {
+          throw error;
+        }
+      } catch (error) {
+        console.log("Error fetching requests", error.message);
+      }
+    }
+
+    fetchRequests();
+  }, []);
+
+  const [open, setOpen] = React.useState(true);
+  const router = useRouter();
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
+
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar position="absolute" open={open}>
+          <Toolbar
             sx={{
-              backgroundColor: (theme) =>
-                theme.palette.mode === "light"
-                  ? theme.palette.grey[100]
-                  : theme.palette.grey[900],
-              flexGrow: 1,
-              height: "100vh",
-              overflow: "auto",
+              pr: "24px", // keep right padding when drawer closed
             }}
           >
-            <Toolbar />
-            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={toggleDrawer}
+              sx={{
+                marginRight: "36px",
+                ...(open && { display: "none" }),
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              sx={{ flexGrow: 1 }}
+            >
+              My Requests
+            </Typography>
+            <IconButton color="inherit">
+              <Badge badgeContent={4} color="secondary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Drawer variant="permanent" open={open}>
+          <Toolbar
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "flex-end",
+              px: [1],
+            }}
+          >
+            <IconButton aria-label="close drawer" onClick={toggleDrawer}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </Toolbar>
+          <Divider />
+          <List component="nav">
+            {/* Your existing list items */}
+            <ListItem
+              button
+              aria-label="add request"
+              onClick={() => router.push("/add-request")}
+            >
+              <ListItemIcon>
+                <AddIcon /> {/* Icon for adding */}
+              </ListItemIcon>
+              <ListItemText primary="Add Request" />
+            </ListItem>
+          </List>
+        </Drawer>
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: "100vh",
+            overflow: "auto",
+          }}
+        >
+          <Toolbar />
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
             <Grid container spacing={1}>
-  <         Grid item xs={12}>
-            <Box
-                    sx={{
-                      p: 2,
-                      display: "flex",
-                      justifyContent:"space-between",
-                      flexDirection: "row",
-                    }}
-                  >
-            <Typography variant="subtitle1">Subject</Typography>
-            <Typography variant="subtitle1">Type</Typography>
-            <Typography variant="subtitle1">Assigned To</Typography>
-            <Typography variant="subtitle1">Status</Typography>
-                  </Box>
-            </Grid>
+              <         Grid item xs={12}>
+                <Box
+                  sx={{
+                    p: 2,
+                    display: "flex",
+                    justifyContent: "space-between",
+                    flexDirection: "row",
+                  }}
+                >
+                  <Typography variant="subtitle1">Subject</Typography>
+                  <Typography variant="subtitle1">Type</Typography>
+                  <Typography variant="subtitle1">Assigned To</Typography>
+                  <Typography variant="subtitle1">Status</Typography>
+                </Box>
+              </Grid>
             </Grid>
             <Divider />
-              <Grid container spacing={2}>
-                {/* Chart */}
-                <Grid item xs={12}>
-                  <Box
-                    sx={{
-                      p: 2,
-                      display: "flex",
-                      flexDirection: "column",
-                    }}
-                  >
-            {rows.map((row, index) => (
-            <React.Fragment key={row.id}>
-              <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: 1 }}>
-                <Typography>{row.Subject}</Typography>
-                <Typography>{row.Type}</Typography>
-                <Typography>{row.AssignedTo}</Typography>
-                <StatusIndicator status={row.status} />
-              </Box>
-              {/* Adding a Divider except after the last row */}
-              {index < rows.length - 1 && <Divider />}
-            </React.Fragment>
-          ))}
+            <Grid container spacing={2}>
+              {/* Chart */}
+              <Grid item xs={12}>
+                <Box
+                  sx={{
+                    p: 2,
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  {requests.map((request, index) => (
+                    <React.Fragment key={index}>
+                      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: 1 }}>
+                        <Typography>{request.subject}</Typography>
+                        <Typography>{request.type}</Typography>
+                        <Typography>{request.assigned_to}</Typography>
+                        <StatusIndicator status={request.status} />
+                      </Box>
+                      {/* Adding a Divider except after the last row */}
+                      {index < requests.length - 1 && <Divider />}
+                    </React.Fragment>
+                  ))}
 
-                  </Box>
-                </Grid>
+                </Box>
               </Grid>
-              <Copyright sx={{ pt: 4 }} />
-            </Container>
-          </Box>
+            </Grid>
+            <Copyright sx={{ pt: 4 }} />
+          </Container>
         </Box>
-      </ThemeProvider>
-    );
-  }
-  
+      </Box>
+    </ThemeProvider>
+  );
+};
+
+
+
+export default userRequests;
