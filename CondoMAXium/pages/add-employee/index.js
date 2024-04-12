942362bd8f8e34871ddab8736baf07b5547bb83e
dlu2002 <al_delu@live.concordia.ca>
2024-04-12 12:24:40 -0400

    Manage, add and edit employees
    - Created logic for managing employees
    - Takes information from employees database and displays it
    - Allows users to add an employee with these fields: first name, last name, email, phone number, and position
    - Two positions: Financial analyst and Daily Operations Analyst (manager counts as CMC)
    - Checks if email is already in the table
    - Edit employee function: allows to change employee fields for an employee in the table

---
+++ CondoMAXium/pages/add-employee/index.js
@@ -0,0 +1,191 @@
+import {
+    Button,
+    Container,
+    Grid,
+    Box,
+    InputLabel,
+    MenuItem,
+    Select,
+    TextField,
+    FormControl,
+    Typography,
+    InputAdornment
+} from "@mui/material";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import supabase from "../../config/supabaseClient";
+import styles from "../../styles/units.module.css";
+import {v4 as uuidv4} from 'uuid';
+  
+  
+const {
+    data: { user },
+} = await supabase.auth.getUser();
+  
+const AddEmployeeForm = () => {
+    const [first_name, setFirstName] = useState("");
+    const [last_name, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone_number, setPhoneNumber] = useState("");
+    const [position, setPosition] = useState("");  
+
+    // Method to handle position selection
+    const handlePositionChange = (event) => {
+        setPosition(event.target.value);
+    };
+  
+    //handle method for phone number
+    const handlePhoneChange = (event) => {
+        // Format the phone number as the user types
+        const formattedPhoneNumber = formatPhoneNumber(event.target.value);
+        setPhoneNumber(formattedPhoneNumber);
+    };
+  
+    //correct formatting for phone number
+    const formatPhoneNumber = (input) => {
+        // Remove non-numeric characters
+        const numericInput = input.replace(/\D/g, '');
+        
+        // Apply the desired pattern (XXX-XXX-XXXX)
+        const formattedPhoneNumber = numericInput.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3');
+    
+        return formattedPhoneNumber;
+    };
+  
+    const handleAdd = async (event) => {
+        event.preventDefault();
+    
+        try {
+            // Check if the email already exists
+            const { data: existingEmployee, error: checkError } = await supabase
+                .from('employees')
+                .select('email')
+                .eq('email', email)
+                .maybeSingle();
+    
+            if (checkError) throw checkError;
+    
+            if (existingEmployee) {
+                alert('An employee with this email already exists.');
+                return;
+            }
+    
+            // Insert new employee
+            const { data: employeeData, error: insertError } = await supabase.from("employees").insert([
+                {
+                    first_name: first_name,
+                    last_name: last_name,
+                    email: email,
+                    position: position, // Ensure this matches the enum values in the database
+                    phone_number: phone_number,
+                }
+            ]);
+    
+            if (insertError) throw insertError;
+    
+            console.log("Successfully added employee: ", employeeData);
+            alert('Employee added successfully!');
+            router.push("/manage-employees");
+        } catch (error) {
+            console.error("Error in operation: ", error.message);
+            alert('Failed to add employee: ' + error.message);
+        }
+    };
+    
+    const router = useRouter();
+  
+    return (
+        <Box className={styles.outsideContainer}>
+            <Container className={styles.unitsContainer}>
+    
+            <Typography variant="h4" gutterBottom className={styles.editUnitsHeader}>
+                Add Employee
+            </Typography>
+            <Typography 
+                variant="h5" 
+                gutterBottom 
+                sx={{textDecoration: 'underline'}} 
+                display="inline" 
+                style={{ color:"#333", padding: "2%" }}
+            >
+                User Information
+            </Typography>
+                <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                        <TextField
+                        name="first_name"
+                        label="First Name"
+                        value={first_name}
+                        onChange={(event) => setFirstName(event.target.value)}
+                        fullWidth
+                        margin="normal"
+                        required
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <TextField
+                        name="last_name"
+                        label="Last Name"
+                        value={last_name}
+                        onChange={(event) => setLastName(event.target.value)}
+                        fullWidth
+                        margin="normal"
+                        required
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <TextField
+                        name="email"
+                        label="Email"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                        fullWidth
+                        margin="normal"
+                        required
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <TextField
+                        name="phone_number"
+                        label="Phone Number"
+                        value={phone_number}
+                        onChange={handlePhoneChange}
+                        fullWidth
+                        margin="normal"
+                        required
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <FormControl fullWidth>
+                            <InputLabel id="position-select-label">Position</InputLabel>
+                            <Select
+                                labelId="position-select-label"
+                                id="position-select"
+                                value={position}
+                                label="Position"
+                                onChange={handlePositionChange}
+                            >
+                                <MenuItem value="financial">Financial Analyst</MenuItem>
+                                <MenuItem value="daily_op">Daily Operations Analyst</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Grid>
+                </Grid>
+                <br/>
+                <br/>
+                <Grid container justifyContent="center" style={{ marginTop: "20px" }}>
+                    <Button
+                        variant="contained"
+                        size="large"
+                        onClick={handleAdd}
+                    >
+                        Save
+                    </Button>
+                </Grid>
+            </Container>
+      </Box>
+    );
+};
+
+export default AddEmployeeForm;
+  
---
+++ CondoMAXium/pages/manage-employees/index.js
@@ -0,0 +1,189 @@
+// ProfilePage.tsx
+import React, { useEffect, useState } from "react";
+// import { useNavigate } from "react-router-dom";
+import { useRouter } from "next/router";
+// import Header from "../../components/layout/Header";
+
+import {
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  TextField,
+  Grid,
+  FormControl,
+  FormControlLabel,
+  InputLabel,
+  MenuItem,
+  Select,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography
+} from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import supabase from "../../config/supabaseClient";
+import styles from "../../styles/units.module.css";
+
+const positionTitles = {
+    financial: "Financial Analyst",
+    daily_op: "Daily Operations Analyst"
+};
+  
+
+const ManageEmployeesPage = () => {
+    const [employees, setEmployees] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    //fetching employees from database
+    useEffect(() => {
+        const fetchEmployees = async () => {
+            try {
+            const { data, error } = await supabase.from('employees').select('*');
+            if (error) {
+                console.error("Error fetching employees", error.message);
+                alert("Failed to fetch employees: " + error.message);
+                setEmployees([]);
+            }  
+            console.log("Fetched employees:", data); // Check what is being received
+            setEmployees(data || []);
+            } catch (error) {
+                console.error("Error fetching employees", error.message);
+                setEmployees([]);
+            }
+        };
+        
+        
+        fetchEmployees();
+    }, []);
+    
+
+
+    //to handle search bar
+    const handleSearch = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    // to filter
+    const filteredEmployees = employees.filter(employee =>
+        employee.first_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        employee.last_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        employee.email.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        (employee.role && employee.role.toLowerCase().includes(searchTerm.toLowerCase())) ||
+        employee.phone_number.includes(searchTerm)
+    );
+    
+    const router = useRouter();
+
+
+    // Route to the edit-employee page
+    const handleEditEmployeeClick = (employeeId) => {
+        console.log('Editing employee with ID:', employeeId);
+        router.push({
+            pathname: '/edit-employee', // Adjusted to point to the edit-employee page
+            query: { id: employeeId } // Changed query parameter to 'id' for clarity and consistency
+        });
+    }
+    
+    // Route to add a new employee page
+    const handleAddEmployeeClick = () => {
+        const result = router.push("/add-employee"); // Direct routing to the add-employee page
+        console.log('Adding new employee');
+        // Additional logic can be implemented based on the result, if needed
+    }
+    
+
+    return (
+        <>
+        <Box className={styles.outsideContainer}>
+            <Container className={styles.unitsContainer} 
+                    //CHECK IF "sm" CAUSES ISSUES
+                    maxWidth="sm">
+            <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                <Typography variant="h4" component="h1" className={styles.unitsHeader}>
+                    Employees
+                </Typography>
+                </Grid>
+
+                <Grid item xs={12} sm={6} className={styles.searchGrid}>
+                <Box className={styles.searchBox}>
+                    <TextField
+                    size="small"
+                    type="text"
+                    placeholder="Search here"
+                    onChange={handleSearch}
+                    value={searchTerm}
+                    fullWidth
+                />
+                </Box>
+                <Box textAlign="right" style={{marginRight: '20px'}}>
+                    <Button 
+                    variant="contained" 
+                    startIcon={<AddIcon />} 
+                    onClick={handleAddEmployeeClick} 
+                    >Add Employee</Button>
+                </Box>
+                </Grid>
+                
+                <Grid item xs={12}>
+                <Box className={styles.unitsBox}>
+                    <TableContainer>
+                    <Table className={styles.unitsTable}>
+                        <TableHead>
+                        <TableRow>
+                            <TableCell>
+                            <b>First Name</b>
+                            </TableCell>
+                            <TableCell>
+                            <b>Last Name</b>
+                            </TableCell>
+                            <TableCell>
+                            <b>E-mail</b>
+                            </TableCell>
+                            <TableCell>
+                            <b>Phone Number</b>
+                            </TableCell>
+                            <TableCell>
+                            <b>Position</b>
+                            </TableCell>
+                        </TableRow>
+                        </TableHead>
+                        <TableBody>
+                        {/* print values from the DB */}
+                        {filteredEmployees.map((employee, index) => (
+                            <TableRow key={index}>
+                                <TableCell>{employee.first_name}</TableCell>
+                                <TableCell>{employee.last_name}</TableCell>
+                                <TableCell>{employee.email}</TableCell>
+                                <TableCell>{employee.phone_number}</TableCell>
+                                <TableCell>
+                                    {positionTitles[employee.position] || 'N/A'}
+                                </TableCell>
+                                <TableCell>
+                                <Button 
+                                    variant="contained" 
+                                    size="small"
+                                    startIcon={<EditIcon />} // Ensure EditIcon is imported
+                                    onClick={() => handleEditEmployeeClick(employee.id)}
+                                >Edit</Button>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                        </TableBody> 
+                    </Table>
+                    </TableContainer>
+                </Box>
+                </Grid>
+            </Grid>
+            </Container>
+        </Box>
+        </>
+    );
+};
+
+export default ManageEmployeesPage;
