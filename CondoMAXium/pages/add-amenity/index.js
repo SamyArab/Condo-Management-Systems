a90901c1e59cf3205afe9f8fd26a8da9ee87d0ab
SamyArab <samy013@outlook.com>
2024-04-14 13:06:09 -0400

    added textfields for basic info + submit btn

--- CondoMAXium/pages/add-amenity/index.js
+++ CondoMAXium/pages/add-amenity/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useRouter } from "next/router";
-import { Container, Typography } from "@mui/material";
+import { Container, Typography, Grid, TextField, Button } from "@mui/material";
 
 const AddAmenityForm = () => {
   const [amenityName, setAmenityName] = useState("");
@@ -35,6 +35,12 @@ const AddAmenityForm = () => {
 
   const router = useRouter();
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    //add amenity to db
+  };
+
   return (
     <div style={{ paddingTop: "140px", paddingBottom: "40px" }}>
       {" "}
@@ -42,6 +48,60 @@ const AddAmenityForm = () => {
         <Typography variant="h4" gutterBottom>
           Add Amenity
         </Typography>
+        <form onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <TextField
+                name="amenityName"
+                label="Name"
+                value={amenityName}
+                onChange={(event) => setAmenityName(event.target.value)}
+                fullWidth
+                margin="normal"
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                name="floor"
+                label="Floor Number"
+                value={floor}
+                onChange={(event) => setFloor(event.target.value)}
+                fullWidth
+                margin="normal"
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                name="sqft"
+                label="Square Footage"
+                value={sqft}
+                onChange={(event) => setSqft(event.target.value)}
+                fullWidth
+                margin="normal"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                name="description"
+                label="Description"
+                value={description}
+                onChange={(event) => setDescription(event.target.value)}
+                fullWidth
+                margin="normal"
+              />
+            </Grid>
+          </Grid>
+          <Grid container justifyContent="center" style={{ marginTop: "20px" }}>
+            <Button
+              type="submit"
+              variant="contained"
+              color="inherit"
+              onClick={() => router.push("/profile")}
+            >
+              Submit
+            </Button>
+          </Grid>
+        </form>
       </Container>
     </div>
   );
