21684580135fa94efd524e061f42830cd60da2eb
SamyArab <samy013@outlook.com>
2024-04-30 14:37:15 -0400

    script tag added

--- CondoMAXium/pages/login/index.js
+++ CondoMAXium/pages/login/index.js
@@ -19,6 +19,7 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Head from "next/head";
+import Script from "next/script";
 
 function Copyright(props) {
   return (
@@ -101,10 +102,10 @@ export default function SignInSide() {
   return (
     <>
       <Head>
-        <script
+        <Script
           id="sc-script"
           src="https://cdn.smartcat-proxy.com/60a29c2d1d4341e38fbb9d3f4a3bef3d/script-v1/__translator.js?hash=7e6e37c59d0bf7e0a6f687b25f488757"
-        ></script>
+        ></Script>
       </Head>
       <ThemeProvider theme={defaultTheme}>
         <Grid container component="main" sx={{ height: "100vh" }}>
