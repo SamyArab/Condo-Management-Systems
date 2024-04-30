a3256fd3181d66afd65e187eafd78148cd6c2615
SamyArab <samy013@outlook.com>
2024-04-30 16:37:19 -0400

    adding head tag

--- CondoMAXium/pages/login/index.js
+++ CondoMAXium/pages/login/index.js
@@ -101,10 +101,12 @@ export default function SignInSide() {
 
   return (
     <>
-      <Script
-        id="sc-script"
-        src="https://cdn.smartcat-proxy.com/60a29c2d1d4341e38fbb9d3f4a3bef3d/script-v1/__translator.js?hash=7e6e37c59d0bf7e0a6f687b25f488757"
-      ></Script>
+      <Head>
+        <script
+          id="sc-script"
+          src="https://cdn.smartcat-proxy.com/60a29c2d1d4341e38fbb9d3f4a3bef3d/script-v1/__translator.js?hash=7e6e37c59d0bf7e0a6f687b25f488757"
+        ></script>
+      </Head>
       <ThemeProvider theme={defaultTheme}>
         <Grid container component="main" sx={{ height: "100vh" }}>
           <CssBaseline />
