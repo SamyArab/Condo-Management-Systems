4d4e487af732e0c35739f5f2583bb9c532c027fb
Nicola-Bdewi <74018961+Nicola-Bdewi@users.noreply.github.com>
2024-04-30 16:16:04 -0400

    Adding configurations for pwa

---
+++ CondoMAXium/next.config.js
@@ -0,0 +1,13 @@
+
+
+const withPWA = require('next-pwa')({
+    dest: 'public'
+  })
+  
+module.exports = withPWA({
+    pwa: {
+      dest: "public",
+      register: true,
+      skipWaiting: true,
+    },
+})
