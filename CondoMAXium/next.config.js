0c8e2fc6162bcb7082147219c8173caa95cb350b
Nicola-Bdewi <74018961+Nicola-Bdewi@users.noreply.github.com>
2024-05-01 11:49:49 -0400

    Adding configurations for the mobile app

---
+++ CondoMAXium/next.config.js
@@ -0,0 +1,11 @@
+const withPWA = require('next-pwa')({
+    dest: 'public'
+  })
+  
+module.exports = withPWA({
+    pwa: {
+      dest: "public",
+      register: true,
+      skipWaiting: true,
+    },
+})
